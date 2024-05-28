f1468628e273e4acf8cf8e5084ab482a52c30354
ALI LAFKIH IMRANE <imr07ane@gmail.com>
2024-05-28 00:09:10 +0000

    Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+    allEmployees: {
+      ...employeesList,
+    },
+  };
+}
