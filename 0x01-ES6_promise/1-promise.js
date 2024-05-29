164570c0440c197ba16236cfdb3d69fea9f922f5
ALI LAFKIH IMRANE <imr07ane@gmail.com>
2024-05-29 12:20:50 +0000

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,5 @@
+export default function getFullResponseFromAPI(success) {
+  if(success==true){
+    return Promise.resolve({ status: 200, body: 'Success' });}else{
+    return Promise.reject(new Error('The fake API is not working currently'));
+}}
